df05d59fdd63637b7c1c0f31e0493ef0da2d98ed
“Mars-Droidgit config --global user.name “Mars-Droid <c5321@mail.ru>
2020-11-27 20:48:53 +0500

    T

---
+++ Go1/Go/Go/ChangeCarCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChangeCarCollectionViewCell.swift
+//  Go
+//
+//  Created by Mars-Droid on 27.11.2020.
+//
+
+import UIKit
+
+class ChangeCarCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var carPicture: UIImageView!
+    
+}
---
+++ Go1/Go/Go/ResultTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ResultTableViewCell.swift
+//  Go
+//
+//  Created by Mars-Droid on 27.11.2020.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namePlayer: UILabel!
+    
+    @IBOutlet weak var gameResult: UILabel!
+    
+    @IBOutlet weak var timeResult: UILabel!
+}
